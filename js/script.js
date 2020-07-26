9e36bf1f8c9214c3c59b52c43f5369b0010ef9da
stas <plps12kovsm@gmail.com>
2020-07-26 21:53:49 +0300

    one

---
+++ js/script.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/* let persone = {
+    name: "john",
+    age: 25,
+    isMarried: false
+};
+ console.log(persone.name);
+
+ */
+//alert('hello World');
+
+//let answer = confirm('are you here&');
+
+//console.log(answer);
+/* let answer = +prompt('Есть ли Вам 18', 'Да');
+
+console.log(typeof (answer));
+
+console.log("arr" + "-object");
+
+console.log(4 + "-object"); */
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
