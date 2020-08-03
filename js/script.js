091e06c2414c269188aea051c573f9e64796f266
stas <plps12kovsm@gmail.com>
2020-08-03 22:03:42 +0300

    3task

--- js/script.js
+++ js/script.js
@@ -31,6 +31,11 @@ console.log(4 + "-object"); */
 //console.log(decr);
 
 //Условия
+/* if (2 * 4 == 4) {
+    console.log("верно")
+} else {
+    console.log("не верно")
+} */
 /* let num = 50;
 if (num < 49) {
     console.log("bed")
@@ -38,9 +43,9 @@ if (num < 49) {
     console.log("much")
 } else {
     console.log("good")
-};
+}; */
 
-switch (num) {
+/* switch (num) {
     case num < 49:
         console.log("bed");
         break;
@@ -59,7 +64,8 @@ switch (num) {
 
 } */
 //First task
-let money = prompt("Ваш бюджет на месяц?", ''),
+//задание 1
+/* let money = prompt("Ваш бюджет на месяц?", ''),
     time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
 let appData = {
@@ -77,10 +83,138 @@ let a1 = prompt("Введите обязательную статью расхо
     a4 = prompt("Во сколько обойдется?", '');
 
 appData.expenses[a1] = a2;
-appData.expenses[a3] = a4;
+appData.expenses[a3] = a4; */
 
 // Важно! В последних версиях браузеров поведение работы со свойствами объектов поменялось. 
 // Теперь синтаксис через точку может не работать. Поэтому выше я использовал квадратные скобки. 
 // Если вы это читаете - то в скором времени обновится и видео в самом курсе. Спасибо за внимание !
 
-alert(appData.budget / 30);
+/* alert(appData.budget / 30); */
+
+//цикл
+//let num = 50;
+/* while (num < 55) {
+    console.log(num);
+    num++;
+} */
+/* do {
+    console.log(num);
+    num++;
+} */
+/* while (num < 55);
+
+for (let i = 1; i < 8; i++) {
+    if (i == 6) {
+        break
+    }
+    console.log(i)
+} */
+
+//задание 2
+/* let x = 5;
+alert(x++);
+console.log(x) */
+
+/* let y = 1;
+let x = y = 2;
+alert(x) */
+
+
+/* alert("1" [0]) */
+
+//alert(null || 2 && 3 || 4);
+
+//alert(+"Infinity");
+
+
+//задание 3
+let money = +prompt("Ваш бюджет на месяц?", ""),
+    time = prompt("Введите дату в формате YYYY-MM-DD", "");
+
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+};
+
+
+for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+        b = prompt("Во сколько обойдется?", "");
+
+    if (typeof (a) === 'string' && typeof (a) != null && typeof (b) != null && a != "" && b != "" && a.length < 50) {
+
+        console.log("done");
+
+        appData.expenses[a] = b;
+    } else {
+        console.log("bad result");
+        i--;
+    }
+
+};
+
+
+
+// Используем цикл WHILE
+
+// let i = 0;
+// while (i < 2) {
+//     let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
+//         b = prompt ("Во сколько обойдется?", "");
+
+//     if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
+
+//         console.log ("done");
+
+//         appData.expenses[a] = b;
+//     } else {
+//          console.log ("bad result");
+//          i--;
+//     }
+
+//     i++;
+// }
+
+
+
+// Используем цикл DO...WHILE
+
+// let i = 0;
+// do {
+//     let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
+//         b = prompt ("Во сколько обойдется?", "");
+
+//     if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
+
+//         console.log ("done");
+
+//         appData.expenses[a] = b;
+//     } else {
+//          console.log ("bad result");
+//          i--;
+//     }
+
+//     i++;
+// }
+// while(i < 2);
+
+
+appData.moneyPerDay = appData.budget / 30;
+
+
+alert("Бюджет на 1 день составляет " + appData.moneyPerDay + "руб.");
+
+
+if (appData.moneyPerDay < 100) {
+    console.log("Это минимальный уровень достатка!");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("Это средний уровень достатка!");
+} else if (appData.moneyPerDay > 2000) {
+    console.log("Это высокий уровень достатка!");
+} else {
+    console.log("Произошла ошибка");
+}
